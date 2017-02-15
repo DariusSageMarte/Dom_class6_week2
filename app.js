f873c3dd127148936bdec1e72a01cac36f4fa214
Darius Marte <Darius.marte66gmail.com>
2017-02-14 20:56:55 -0500

    num_on_page

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 'use strict';
-var storeHours = [6,7,8,9,10,11,12,1,2,3,4,5,6,7,8];
+var storeHours = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm', '3pm','4pm','5pm','6pm','7pm','8pm'];
 
 function Store(shop, minCust, maxCust, cookiePerCust) {
   this.name = shop;
@@ -16,7 +16,7 @@ Store.prototype.cookiesPerHour = function () {
   return (this.cookiePerCust * this.randomNum());};
 Store.prototype.customerCookies = function () {
   for(var i = 0; i < storeHours.length; i++ ) {
-     this.zeroDay.push(this.cookiesPerHour());    }
+    this.zeroDay.push(this.cookiesPerHour()); }
 };
 
 
@@ -26,3 +26,12 @@ var seaTacAir = new Store ('SeaTac Airport' , 3, 24, 1.2);
 var seattleCenter = new Store ('Seattle Center', 11, 38, 3.7);
 var capHill = new Store ('Capitol Hil', 20, 38, 2.3);
 var alki = new Store ('Alki', 2, 16, 4.6);
+
+var hourTable = document.getElementById('tom');
+var hourTr = document.createElement('tr');
+hourTable.appendChild(hourTr);
+for (var i = 0; i < storeHours.length; i++ ) {
+  var hourTh = document.createElement('th');
+  hourTh.textContent = storeHours[i];
+  hourTr.appendChild(hourTh);
+}
